5af56e8158d0232c5bc07ac3bf77178fb7668cf1
Razvan <it@devpro.solutions>
2025-08-19 11:12:51 +0300

    Add a sleep to lswsparse service

--- conversion/service.sh
+++ conversion/service.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 while true; do
-sysctl fs.enforce_symlinksifowner=1
+# only for cloudlinux: sysctl fs.enforce_symlinksifowner=1
 systemctl stop httpd
 systemctl disable httpd
 cd /usr/local/lsws/configparse/
 php service.php
+sleep 10s
 done
